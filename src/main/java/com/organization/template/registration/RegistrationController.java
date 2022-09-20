e7b37a7452cfd6db6a0b69641099cd963ca5befe
Maxim <maximgrigorovua@gmail.com>
2022-09-20 17:02:03 +0300

    Fix bad request

--- src/main/java/com/organization/template/registration/RegistrationController.java
+++ src/main/java/com/organization/template/registration/RegistrationController.java
@@ -1,26 +1,50 @@
 package com.organization.template.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "product/registration")
 @AllArgsConstructor
+@Validated
 public class RegistrationController {
     private final RegistrationService registrationService;
 
     /**
      * Register new User
+     *
      * @param request (firstName, lastName, password, email, role)
      * @return user token
      */
     @PostMapping
-    public String register(@RequestBody RegistrationRequest request) {
+    public String register(@Valid @RequestBody RegistrationRequest request) {
         return registrationService.register(request);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidatorException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }));
+
+        return errors;
     }
 
     /**
      * Confirm user token
+     *
      * @param token user
      * @return confirmation page
      */
